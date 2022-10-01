974712b6656692a096513bd5c2b90ff86699b018
NikolasPasichnik <nikolas.pasichnik@mail.mcgill.ca>
2022-10-01 16:50:23 -0400

    added dataset

--- MushromPandasWork.py
+++ MushromPandasWork.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-csv_directory = r"C:\Users\nikol\OneDrive\Documents\ConsumeUrDoom\mushrooms.csv"
+csv_directory = r"mushrooms.csv"
 dataset = pd.read_csv(csv_directory)
 
 for col in dataset: 

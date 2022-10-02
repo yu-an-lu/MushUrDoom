6d0f3bb75c0d24d29a79e4ece794783e2079387f
Yu An Lu <yu.a.lu@mail.mcgill.ca>
2022-10-02 04:23:32 -0400

    Created Model Class and predict()

--- MushroomML.py
+++ MushroomML.py
@@ -1,53 +1,81 @@
-import os
+from tkinter import Y
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import pandas as pd
 import numpy as np
-import os
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import os
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
 #Source for the ML code: https://www.milindsoorya.com/blog/mushroom-dataset-analysis-and-classification-python
 
-df = pd.read_csv('mushrooms.csv')
+class Model:
+    def __init__(self):
+        self.process_data()
+        self.dt = self.train()
+        
+    def process_data(self):
+        df = pd.read_csv('mushrooms.csv')
+        df = df.astype('category')
+        df = df.drop(["veil-type"],axis=1)
 
-df = df.astype('category')
-df = df.drop(["veil-type"],axis=1)
+        #Getting a test case 1
+        df_test1 = df.iloc[0]
+        df_test1 = df_test1.to_numpy()
+        df_test1 = np.delete(df_test1, 0)
+        #Getting a test case 2
+        df_test2 = df.iloc[1]
+        df_test2 = df_test2.to_numpy()
+        df_test2 = np.delete(df_test2, 0)
 
-#Getting a test case 1
-df_test1 = df.iloc[0]
-df_test1 = df_test1.to_numpy()
-df_test1 = np.delete(df_test1, 0)
-#Getting a test case 2
-df_test2 = df.iloc[1]
-df_test2 = df_test2.to_numpy()
-df_test2 = np.delete(df_test2, 0)
+        labelencoder=LabelEncoder()
 
-labelencoder=LabelEncoder()
+        for column in df.columns:
+            df[column] = labelencoder.fit_transform(df[column])
 
-for column in df.columns:
-    df[column] = labelencoder.fit_transform(df[column])
+        # print([df_test1][0])
+        # print([df_test2][0])
 
-#Test Case 
-df_test1 = labelencoder.fit_transform(df_test1)
-df_test2 = labelencoder.fit_transform(df_test2)
+        #Test Case 
+        self.df_test1 = labelencoder.fit_transform(df_test1)
+        self.df_test2 = labelencoder.fit_transform(df_test2)
 
-# "class" column as numpy array.
-y = df["class"].values
-# All data except "class" column.
-x = df.drop(["class"], axis=1).values
-# Split data for train and test.
-x_train, x_test, y_train, y_test = train_test_split(x,y,random_state=42,test_size=0.2)
-dt = DecisionTreeClassifier()
-dt.fit(x_train,y_train)
+        # "class" column as numpy array.
+        y = df["class"].values
+        # All data except "class" column.
+        x = df.drop(["class"], axis=1).values
+        
+        # Split data for train and test.
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x,y,random_state=42,test_size=0.2)
+        
+        
+    def train(self):
+        dt = DecisionTreeClassifier()
+        dt.fit(self.x_train, self.y_train)
 
-#Test Cases 
-prediction1 = dt.predict([df_test1])[0]
-prediction2 = dt.predict([df_test2])[0]
+        #Test Cases 
+        prediction1 = dt.predict([self.df_test1])[0]
+        prediction2 = dt.predict([self.df_test2])[0]
 
-print(prediction1) #Giving 1 (poisonous)
-print(prediction2) #Giving 0 (edible) 
 
-print("Test Accuracy: {}%".format(round(dt.score(x_test,y_test)*100,2)))
+        print(prediction1) #Giving 1 (poisonous)
+        print(prediction2) #Giving 0 (edible)
+
+        print("Test Accuracy: {}%".format(round(dt.score(self.x_test, self.y_test)*100,2)))
+        
+        return dt
+    
+    def predict(self, data):
+        # encode data
+        labelencoder=LabelEncoder()
+        data = labelencoder.fit_transform(data)
+        
+        # predicts
+        prediction = self.dt.predict(data)
+        
+        return prediction
+
+# To remove
+if __name__ == "__main__":
+    model = Model()
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from sklearn.preprocessing import LabelEncoder
+from MushroomML import Model
 
 # Create a Flask instance
 app = Flask(__name__)
@@ -7,13 +9,52 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/test')
-def test():
+@app.route('/predict')
+def predict():
+    # attributes
+    cap_shape = request.args.get('cap_shape')
+    cap_surface = request.args.get('cap_surface')
+    cap_color = request.args.get('cap_color')
+    bruises = request.args.get('bruises')
+    odor = request.args.get('odor')
+    gill_attach = request.args.get('gill_attach')
+    gill_space = request.args.get('gill_space')
+    gill_size = request.args.get('gill_size')
+    gill_color = request.args.get('gill_color')
+    stalk_shape = request.args.get('stalk_shape')
+    stalk_root = request.args.get('stalk_root')
+    stalk_surface_above = request.args.get('stalk_surface_above')
+    stalk_surface_below = request.args.get('stalk_surface_below')
+    stalk_color_above = request.args.get('stalk_color_above')
+    stalk_color_below = request.args.get('stalk_color_below')
+    veil_color = request.args.get('veil_color')
+    ring_number = request.args.get('ring_number')
+    ring_type = request.args.get('ring_type')
+    spore_print_color = request.args.get('spore_print_color')
+    population = request.args.get('population')
+    habitat = request.args.get('habitat')
     
-    person = request.args.get('person')
-    color = request.args.get('color')
-    # add attribute here
     
-    data = {"hello": person, "color": color, "foo": "bar"}
-    return data
-
+    data = {'cap-shape': cap_shape, 'cap-surface': cap_surface, 
+            'cap-color': cap_color, 'bruises': bruises, 'odor': odor,
+            'gill-attachment': gill_attach, 'gill-spacing': gill_space,
+            'gill-size': gill_size, 'gill-color': gill_color,
+            'stalk-shape': stalk_shape, 'stalk-root': stalk_root,
+            'stalk-surface-above-ring': stalk_surface_above,
+            'stalk-surface-below-ring': stalk_surface_below,
+            'stalk-color-above-ring': stalk_color_above,
+            'stalk-color-below-ring': stalk_color_below,
+            'veil-color': veil_color, 'ring-number': ring_number,
+            'ring-type': ring_type, 'spore-print-color': spore_print_color,
+            'population': population, 'habitat': habitat}
+    
+    # get prediction
+    labelencoder=LabelEncoder()
+    data = labelencoder.fit_transform(data)
+    
+    # use trained decision tree
+    model = Model.__init__()
+    prediction = model.predict(data)
+    
+    # return prediction
+    return prediction
